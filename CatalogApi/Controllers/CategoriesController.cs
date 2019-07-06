15f42f1ab4800daa7d46cd6714db0a8e1532b24a
Tharindu Sandamal Keerthirathna <tarindusandamal@gmail.com>
2019-07-06 18:57:25 +0530

    Initial checking

---
+++ CatalogApi/Controllers/CategoriesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Category>> Get()
+        {
+            return Ok(new Category[]
+            {
+                new Category { Id = 1, Name = "cat-1" },
+                new Category { Id = 2, Name = "cat-2" }
+            });
+        }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ProductApi/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            return Ok(new Product[]
+            {
+                new Product { Id = 1, Name = "product-1" },
+                new Product { Id = 2, Name = "product-2" },
+                new Product { Id = 3, Name = "product-3" }
+            });
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
